34bb4306c35dd593d394080044bfff7c51d3a134
Unesco <younes.tahiri6@gmail.com>
2021-07-24 16:23:53 +0100

    Created new project (Matchday)

---
+++ main.js
@@ -0,0 +1,22 @@
+
+function searchLeft() {
+var teamLeft = document.getElementById('teamfull'),
+
+	inputLeft = document.getElementById('inputLeft').value,
+
+	myImg = document.querySelectors('img').value,
+
+	inputLeft = myImg;
+
+	teamLeft.innerHTML = 
+};
+/*
+var = 	myLogo = document.getElementById('myLogo'),
+
+		myParam = document.getElementById('param'),
+
+		teamRight = document.getElementById('teamfull2'),
+
+		inputRight = document.getElementById('inputRight'); 
+
+		*/
